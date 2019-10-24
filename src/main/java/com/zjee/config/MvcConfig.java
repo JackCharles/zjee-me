8ce3b69b777da7dbc7462b7c773a64059cb5f2c7
zhongjie03 <zhongjie03@meituan.com>
2019-10-24 10:24:50 +0800

    redirect 80 to 443

--- src/main/java/com/zjee/config/MvcConfig.java
+++ src/main/java/com/zjee/config/MvcConfig.java
@@ -1,6 +1,11 @@
 package com.zjee.config;
 
 import com.zjee.filter.IpCountFilter;
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
@@ -14,11 +19,12 @@ public class MvcConfig implements WebMvcConfigurer {
     /**
      * 注意：Filter属于Servlet层级，而interceptor属于SpringMVC层级，
      * 它归SpringMVC管理，而Filter归容器直接管理，因此两者配置方式不一致
+     *
      * @param myFilter
      * @return
      */
     @Bean
-    FilterRegistrationBean getFilterRegistrationBean(IpCountFilter myFilter){
+    FilterRegistrationBean getFilterRegistrationBean(IpCountFilter myFilter) {
         FilterRegistrationBean filterRegistrationBean = new FilterRegistrationBean(myFilter);
         filterRegistrationBean.addUrlPatterns("/*");
         filterRegistrationBean.setName("IpCountFilter");
@@ -29,5 +35,32 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
 
+    }
+
+    @Bean
+    public Connector connector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setPort(80);
+        connector.setSecure(false);
+        connector.setRedirectPort(443);
+        return connector;
+    }
+
+    @Bean
+    public TomcatServletWebServerFactory tomcatServletWebServerFactory(Connector connector) {
+        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory() {
+            @Override
+            protected void postProcessContext(Context context) {
+                SecurityConstraint securityConstraint = new SecurityConstraint();
+                securityConstraint.setUserConstraint("CONFIDENTIAL");
+                SecurityCollection securityCollection = new SecurityCollection();
+                securityCollection.addPattern("/*");
+                securityConstraint.addCollection(securityCollection);
+                context.addConstraint(securityConstraint);
+            }
+        };
+        factory.addAdditionalTomcatConnectors(connector);
+        return factory;
     }
 }
