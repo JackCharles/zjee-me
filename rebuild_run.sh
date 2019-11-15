ddcd1c4f6e583081bbe757a386dd301f17a38f7c
unknown <zj2011@live.com>
2019-11-15 22:44:09 +0800

    update deploy script

--- rebuild_run.sh
+++ rebuild_run.sh
@@ -12,6 +12,9 @@ mvn clean
 echo 'compile and packaging jar...'
 mvn package -Dmaven.test.skip=true
 
+echo 'copy native lib...'
+/bin/cp -rf native-lib/libsigar-amd64-linux.so /usr/lib64
+
 echo 'start application...'
 nohup java -jar -Dserver.port=443 target/zjee-ml-1.0.jar &
 
