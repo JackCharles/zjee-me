00d9ea300ef6a0f52adfbd6ef7bfd1cf74e8329c
unknown <zj2011@live.com>
2019-11-15 21:47:07 +0800

    change vps subID

--- src/main/java/com/zjee/constant/Constant.java
+++ src/main/java/com/zjee/constant/Constant.java
@@ -19,7 +19,7 @@ public class Constant {
     public static String VULTR_KEY = "ECPZBD2X6TLYOFQDQG6MUKUU4EUVGOW4VL5Q";
     public static String SERVER_LIST = "https://api.vultr.com/v1/server/list";
     public static String BAND_WIDTH = "https://api.vultr.com/v1/server/bandwidth";
-    public static String SUB_ID = "25171252";
+    public static String SUB_ID = "31211880";
 
     public static String LEAN_CLOUD_APP_ID = "hiuyEvHNGJv0HBzIgke1FOa2-MdYXbMMI";
     public static String LEAN_CLOUD_APP_KEY = "W2WW9ah2D4zfyhW3aYtKw3xD";
