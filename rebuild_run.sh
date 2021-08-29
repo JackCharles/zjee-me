ed105c7dfd57c04e6e7119703af067c0bcb58c89
zhongjie <zjee@live.com>
2021-08-29 19:17:18 +0800

    bug fix

--- rebuild_run.sh
+++ rebuild_run.sh
@@ -13,9 +13,6 @@ mvn clean
 echo 'compile and packaging jar...'
 mvn package -Dmaven.test.skip=true
 
-echo 'copy native lib...'
-/bin/cp -rf native-lib/libsigar-amd64-linux.so /usr/lib64
-
 echo 'start application...'
 nohup java -Xdebug -Xrunjdwp:server=y,transport=dt_socket,address=8418,suspend=n -jar -Dfile.encoding=UTF-8 -Dserver.port=80 target/zjee-me-1.0.jar &
 
